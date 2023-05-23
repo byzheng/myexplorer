b0d1d79232e1b7bd83621d1c8519bb4c5e82142d
Bangyou Zheng <bangyou.zheng@csiro.au>
2023-05-23 12:47:05 +1000

    Open drive from nfs

--- myexplorer.cpp
+++ myexplorer.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <shlwapi.h>
 
+std::string replace_all(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+    }
+    return str;
+}
+
 //    defining main with arguments
 int main(int argc, char** argv)
 {
@@ -20,6 +29,15 @@ int main(int argc, char** argv)
     }
 
     path.erase(0, prefix.length());
+
+    // NFS folder start with "//"
+    if (path.rfind("fs1", 0) == 0) {
+        path = "\\\\" + replace_all(path, "/", "\\");
+        path = replace_all(path, "%7B", "{");
+        path = replace_all(path, "%7D", "}");
+    }
+
+    std::cout << path << "\n";
     const char* file_path = path.c_str();
     // Open folder if it is existed
     if (PathIsDirectoryA(file_path)) {
